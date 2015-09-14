419155136fd8a9b5372f6a296ff320cf0ea9339c
Sergey Romanov <xxsmotur@gmail.com>
2015-09-14 21:28:09 +0500

    Update

--- lsboost.py
+++ lsboost.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.optimiza import fmin
 
 class LSBoost:
     def __init__(self):
@@ -7,10 +8,18 @@ class LSBoost:
     def addHypothesis(self, func):
         self.hyp.append(func)
 
+    def _find_mult(self, p):
+        return self._loss(self.hypprev - p * self.gr, self.y)
+
     def fit(self, X, y):
+        assert(len(self.hyp) > 0)
         hypprev = self.hyp[0](X)
         n = X.shape[0]
+        self.y = y
         for hyp in range(1, self.hyp):
             ydot = [y[i] - hypprev(X[i]) for i in range(n)]
-            hypprev = hypprev + self.grad(a)
+            loss1 = hypprev - self.grad(ydot)
+            self.hypprev = hypprev
+            p = fmin(self._find_mult)
+            hypprev = hypprev + p * self.grad(a)
 
